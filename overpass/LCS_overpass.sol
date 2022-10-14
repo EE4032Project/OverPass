407dbdb1e4e7db381bbed11d737976f0d010f36b
Changrui <78465180+Ch40gRv1-Mu@users.noreply.github.com>
2022-10-14 22:38:42 +0800

    add advise() and getIncentive()

--- overpass/LCS_overpass.sol
+++ overpass/LCS_overpass.sol
@@ -6,21 +6,18 @@ import "./overpass.sol";
 
 contract LCSOverPass is OverPass{
 
-    constructor(uint256 initialSupply) ERC20("LCSOverPass", "LCSOverPass"){
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 initialSupply) OverPass((initialSupply)) {
     }
 
-    mapping (uint256=>uint) results;
 
     // simulate the verification to estimate the gas fee
-    function _simulateVerification(bytes memory s1,  bytes memory s2) private pure returns (bool){
+    function _simulateVerification(bytes memory s1,  bytes memory s2, bytes memory s3, uint k) private pure returns (bool){
         uint checkI = 0;
-        if (s1.length != s1.length) {
+        // simulate length check
+        if (s3.length != k) {
             return false;
         }
-        if (s2.length != s2.length) {
-            return false;
-        }
+        // simulate check over s1
         for (uint i=0; i<s1.length; i++) {
             if (checkI==s1.length) {
                 break;
@@ -34,7 +31,7 @@ contract LCSOverPass is OverPass{
         if (checkI!=s1.length) {
             return false;
         }
-
+        // simulate check over s2
         for (uint i=0; i<s2.length; i++) {
             if (checkI==s2.length) {
                 break;
@@ -52,34 +49,80 @@ contract LCSOverPass is OverPass{
         return true;
     }
 
+    function _verifyResult(bytes memory s1,  bytes memory s2, bytes memory lcs, uint k) private pure returns (bool){
+        require(lcs.length==k, "proof and answers are unmatched.");
+        uint checkI = 0;
+        for (uint i=0; i<s1.length; i++) {
+            if (s1[i]==lcs[checkI]) {
+                checkI +=1;
+                if (checkI==k) {
+                    break;
+                }
+            }
+        }
+        if (checkI!=k) {
+            return false;
+        }
+        checkI = 0;
+        for (uint i=0; i<s2.length; i++) {
+            if (s2[i]==lcs[checkI]) {
+                checkI +=1;
+                if (checkI==k) {
+                    break;
+                }
+            }
+        }
+        if (checkI!=k) {
+            return false;
+        }
+        return true;
+    }
+
     // keccak256(abi.encodePacked("LCS"))
     bytes32 private constant _LCS = 0x816a2b01920faffdf630c9d96404a778baf08493254365c589e0d4366da539ed;
     uint MAX_STR_LEN = 1001;
     // called by other smart contract to compute specified algorithm with parameters
-    function delegate_compute(string[] memory taskParameters) payable public override returns (uint256) {
+    function delegate_compute(string[] memory taskParameters, uint _computePeriod) payable public override returns (uint256) {
         require(taskParameters.length==3 && keccak256(abi.encodePacked((taskParameters[0])))==_LCS, "Wrong delegated task.");
         bytes memory s1  = bytes(taskParameters[1]);
         bytes memory s2 = bytes(taskParameters[2]);
         require(s1.length<MAX_STR_LEN && s2.length<MAX_STR_LEN, "String too long.");
-        _simulateVerification(s1, s2);
+        _simulateVerification(s1, s2, s1, s1.length);
         require(msg.value>tx.gasprice*10, "not enough payment.");
         // set new task
-        tasks[nonce] = Task(msg.sender, nonce, taskParameters, address(0), 0, msg.value, tx.gasprice, false, false);
+
+        tasks[nonce] = Task(msg.sender, nonce, taskParameters, address(0), 0,  msg.value, tx.gasprice, block.number, _computePeriod);
+        
+        emit postNewQuestion(/*taskId*/nonce, /*incentive*/msg.value, /*approxGasFee*/tx.gasprice, _computePeriod);
         nonce += 1;
         return nonce-1;
     }
+
     // return parameters of a function
     function getTaskParameters(uint256 taskId) public view override returns (string[] memory){
         return tasks[taskId].taskParameters;
     }
-    // get the result of Task
-    function completeTask(uint256  taskId) public override {
 
+    function advise(uint256 taskId, uint256 ans, string[] memory proof) payable public override {
+        
+        require(ans>tasks[taskId].bestAnswer, "Better solution exists");
+        require(proof.length==1, "Invalid proof");
+        bytes memory s1 = bytes(tasks[taskId].taskParameters[1]);
+        bytes memory s2 = bytes(tasks[taskId].taskParameters[2]);
+        
+        bool isValidAns = _verifyResult(s1, s2, bytes(proof[0]), ans);
+        require(isValidAns, "Invalid proof");
+        tasks[taskId].bestAdvisor = msg.sender;
+        tasks[taskId].bestAnswer = ans;
+        emit updateQuestionAnswer(taskId, ans, msg.sender);
 
     }
 
-    function advise(uint256 taskId,string[] calldata functionParameters) payable public override {
-
+    function getIncentive(uint256 _taskId) public override winnerOnly(_taskId) {
+        super.transfer(msg.sender, tasks[_taskId].incentive);
+        delete tasks[_taskId];
     }
+
+
 }
 
--- overpass/overpass.sol
+++ overpass/overpass.sol
@@ -10,20 +10,40 @@ abstract contract OverPass is ERC20 {
         uint256 taskId;
         string [] taskParameters;
         address bestAdvisor;
+        uint bestAnswer;
+        uint incentive;
+        uint approxGasFee;
+        uint startBlock;
+        uint computePeriod;
     }
+
+    event postNewQuestion(uint256 taskId, uint incentive, uint approxGasFee, uint computePeriod);
+    event updateQuestionAnswer(uint256 taskId, uint bestAnswer, address bestAdvisor);
+
     // Store the information of tasks
-    mapping(uint256=>Task) private tasks;
+    mapping(uint256=>Task) internal tasks;
+
+    uint internal nonce;
 
     constructor(uint256 initialSupply) ERC20("OverPass", "OP") {
         _mint(msg.sender, initialSupply);
     }
 
+    modifier winnerOnly(uint256 _taskId) {
+        require(
+            block.number>tasks[_taskId].startBlock+tasks[_taskId].computePeriod&&msg.sender == tasks[_taskId].bestAdvisor ,
+            "winner only function"
+        );
+        _;
+    }
+
+    
+
     // called by other smart contract to compute specified algorithm with parameters
-    function delegate_compute(string[] calldata taskParameters) payable public virtual;
+    function delegate_compute(string[] memory taskParameters, uint _computePeriod) payable public virtual returns (uint256);
     // return parameters of a function
-    function getTaskParameters(string calldata taskId)public view virtual;
-    // get the result of Task
-    function completeTask(string calldata functionId) public virtual;
-    function advise(string calldata functionId,string[] calldata functionParameters) payable public virtual;
+    function getTaskParameters(uint256 taskId) public view virtual returns(string[] memory);
+    function advise(uint256 taskId, uint256 ans, string[] memory proof) payable public virtual;
+    function getIncentive(uint256 _taskId) public virtual;
 }
 
