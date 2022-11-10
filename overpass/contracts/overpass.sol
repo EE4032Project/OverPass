400d9bb722ddc1f2a468d615248ea4feb8e8bfb8
Changrui <78465180+Ch40gRv1-Mu@users.noreply.github.com>
2022-11-10 19:15:18 +0800

    add detailed comments for overpass.sol

--- overpass/contracts/overpass.sol
+++ overpass/contracts/overpass.sol
@@ -46,19 +46,24 @@ abstract contract OverPass {
 
     
 
-    // called by other smart contract to compute specified algorithm with parameters
+    // Called by user to delegate compute specific problem
     function delegate_compute(string[] memory taskParameters, uint _computePeriod) payable public virtual returns (uint256);
-    // return parameters of a function
+
+    // Called by miner to advise hint/answer/proof
     function advise(uint256 taskId, uint256 ans, string[] memory proof) payable public virtual;
 
+    // Called by winner miner to get the incentive
     function getIncentive(uint256 _taskId) public virtual;
         function getTaskIncentive(uint256 taskId) public view returns(uint) {
         return tasks[taskId].incentive;
     }
+
+    // Called by miner to get the estimated gas fee
     function getTaskApproxGasFee(uint256 taskId) public view returns(uint){
         return tasks[taskId].approxGasFee;
     }
-        // return parameters of a function
+    
+    // Called by miner return parameters of a function
     function getTaskParameters(uint256 taskId) public view returns (string[] memory){
         return tasks[taskId].taskParameters;
     }
